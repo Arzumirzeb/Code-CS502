0a5be6e80034b56abb4d6043f5485f0ebf77e386
Arzumirzeb <arzumirzeb03@gmail.com>
2024-02-21 16:02:59 +0400

    task-1

---
+++ homework/01.taks_1/script.js
@@ -0,0 +1,36 @@
+////////////// TASK 1 //////////////
+
+// 1.Multiply 12 with 8, and console the result:
+
+// 2.Divide 10 by 2 add result to variables x, and console x:
+
+// 3.Create two variables called num1 and num2, assign the value 20 to num1 and value 17 to num2 then addition(+) them in console.
+
+/* 4.Create variables called name, surname and year assign values to them ('your name', 'your surname', ' your birth of year'),
+  then add them to varibale name (info) and console info. */
+
+// 5.Console the remainder(%) when 17 is divided by 12.
+
+// 6.Create a varibale called "city name" (camelCase). Assign value Baku, then call variable again
+//  and reassign value Gence,console result.
+console.log(12 / 8);
+
+let x = 10 / 2
+console.log(x);
+
+let num1 = 20 
+let num2 = 17
+console.log(num1 + num2);
+
+let name = "Arzu"
+let surname = "Mirzabayova"
+let year = "2003"
+let info = `${name} ${surname} ${year}`
+console.log(info);
+
+console.log(17 % 12);
+
+let cityName = "Baku"
+console.log(cityName);
+cityName = "Gence"
+console.log(cityName);
